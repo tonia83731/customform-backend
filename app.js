738182f7dd21c33305c204db7774088969e1e68f
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2025-03-05 15:59:50 +0800

    feat: fixed cors

--- app.js
+++ app.js
@@ -14,6 +14,23 @@ const routes = require("./routes");
 const port = process.env.PORT || 8080;
 const mongoose_url = process.env.MONGODB_URL;
 
+const allowedOrigins = [
+  "http://localhost:5173", // Development
+  // "https://your-production-frontend.com", // Production
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
 mongoose
   .connect(mongoose_url)
   .then(() => console.log("mongodb connected!"))
@@ -21,7 +38,7 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
